5e3e13fd966f5e38506701d1dc942169ce68a54c
ricardoborrull <ricardoborrull>
2017-12-18 18:50:31 +0100

    Initial commit

---
+++ app/src/main/java/com/example/a2dam/proyectoquicktrade/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.a2dam.proyectoquicktrade;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button login, registrar, users, modify;
+    DatabaseReference bbdd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login = (Button) findViewById(R.id.login);
+        registrar = (Button) findViewById(R.id.registrar);
+        users = (Button) findViewById(R.id.users);
+        modify = (Button) findViewById(R.id.modify);
+
+        bbdd = (FirebaseDatabase.getInstance().getReference("usuario"));
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+        registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, RegistrarActivity.class);
+                startActivity(i);
+            }
+        });
+
+        users.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, UsersActivity.class);
+                startActivity(i);
+            }
+        });
+
+        modify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, RegistrarActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/a2dam/proyectoquicktrade/model/Usuario.java
@@ -0,0 +1,73 @@
+package com.example.a2dam.proyectoquicktrade.model;
+
+/**
+ * Created by 2dam on 18/12/2017.
+ */
+
+public class Usuario {
+
+    private String user;
+    private String nombre;
+    private String apellidos;
+    private String correo;
+    private String direccion;
+
+    public Usuario(String user, String nombre, String apellidos, String correo, String direccion) {
+        this.user = user;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.correo = correo;
+        this.direccion = direccion;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "user='" + user + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", correo='" + correo + '\'' +
+                ", direccion='" + direccion + '\'' +
+                '}';
+    }
+}
